8773e95624ebed1c458c2024078940398b74816b
Philopateer <pitoamir5@gmail.com>
2023-07-28 19:35:57 +0300

    Support ETH

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let api_key = "2BNG5GUPKQEDS4WY3G4T77F4RA1VUCE6CF";
 let base_url = "https://api.etherscan.io/api";
-let address = "0x48e3E4F95CE13fe5427D1c45DC40202c1F614F67";
+let address = '0x48e3E4F95CE13fe5427D1c45DC40202c1F614F67';
 let url = `https://api.etherscan.io/api
 ?module=account
 &action=txlist
@@ -19,12 +19,23 @@ async function Get_Transactions() {
   const data = await response.json();
   const transactions = data.result;
 
-  transactions.forEach(transaction => {
-    let value = parseInt(transaction.value)
-    if(value !== 0){
-        value = value/1000000000000000;
-        value = value/1000;
-        console.log(value)
+  transactions.forEach((transaction) => {
+    let value = parseInt(transaction.value);
+    if (value !== 0) {
+      value = value / 1000000000000000;
+      value = value / 1000;
+      let sender = transaction.from;
+      let reciever = transaction.to;
+      //console.log(`address is ${address}`)
+      //console.log(`sender is ${sender}`);
+      //console.log(`receiver is ${reciever}`);
+
+      if(sender.toLowerCase() === address.toLowerCase()) {
+        console.log(`Mito sent ${value} to ${sender}`)
+      } else {
+        console.log(`Mito recieved ${value} from ${reciever}`)
+      }
+
     }
   });
 }
