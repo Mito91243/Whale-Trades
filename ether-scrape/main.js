ca105b8b351ac3137a6074065fefc5420a76a2ea
Philopateer <pitoamir5@gmail.com>
2023-07-31 18:52:52 +0300

    Updated To Get All Transactions

--- ether-scrape/main.js
+++ ether-scrape/main.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-let address = "0x48e3E4F95CE13fe5427D1c45DC40202c1F614F67";
+let address = "0x6Ec21d1868743a44318c3C259a6d4953F9978538";
 
 function Get_Transactions() {
   axios
@@ -63,11 +63,11 @@ function Get_Transactions_Account() {
 
           //FIX HERE
           let send = sender;
-          let add = address.slice(-8).toLowerCase()
+          let add = address.slice(-8).toLowerCase();
           if (send === add.slice(-8).toLowerCase()) {
             console.log(`SENT ${eth_amount}`);
           } else {
-            console.log(`Recieved ${eth_amount}`);
+            //console.log(`Recieved ${eth_amount}`);
           }
           //TO HERE
         } else {
@@ -75,7 +75,7 @@ function Get_Transactions_Account() {
             .closest("tr")
             .find("td:nth-child(3) > span")
             .text();
-          if (action === "Transfer") {
+          if (eth_amount === 0) {
             Get_Hash_Details(transaction_hash);
           }
         }
@@ -92,6 +92,8 @@ function Get_Hash_Details(hash) {
     if (response.status === 200) {
       //extract html of the response
       const $ = cheerio.load(response.data);
+      const $_ = $;
+
       //loop over each transaction
       $(
         "span.d-inline-flex.flex-wrap.align-items-center > #tokenpricebutton"
@@ -123,7 +125,51 @@ function Get_Hash_Details(hash) {
         let reciever = $(
           "div.card.p-5.mb-3 > div:nth-child(11) > div.col-md-9 > div > span > a"
         ).text();
+        //LOGGING TO SCREEN The Details
+        if (sender.toLowerCase() === address.toLowerCase()) {
+          console.log(
+            `SENT ${alt_amount} ${token} with value of ${alt_value} to ${reciever}`
+          );
+        } else {
+          console.log(
+            `Recieved ${alt_amount} ${token} with value of ${alt_value} from ${sender}`
+          );
+        }
+      });
 
+      //----------------------------------------------------------------------Handles Second Variation of ALT_AMOUNT----------------------------------------------------------------------
+
+      //loop over each transaction
+      $(
+        'div > span.d-inline-flex.flex-wrap.align-items-center > span.text-muted.me-1'
+      ).each((i, el) => {
+        //Get The Value
+        let alt_value = $(el).text();
+
+        //IN THE SAME DIV I NEED TO GET THE ALT
+        let alt_amount = $(el)
+          .closest("span.d-inline-flex.flex-wrap.align-items-center")
+          .find('span.me-1[data-bs-toggle="tooltip"]')
+          .text()
+          .replace(/,/g, "");
+
+        //Get The Token
+        let token = $(el)
+          .closest("span.d-inline-flex.flex-wrap.align-items-center")
+          .find(
+            "a.d-flex.align-items-center > span.text-muted > span.text-truncate"
+          )
+          .text();
+
+        //Get The Sender
+        let sender = $(
+          "div.card.p-5.mb-3 > div:nth-child(10) > div.col-md-9 > div > span > a"
+        ).text();
+
+        //Get The Reciever
+        let reciever = $(
+          "div.card.p-5.mb-3 > div:nth-child(11) > div.col-md-9 > div > span > a"
+        ).text();
         //LOGGING TO SCREEN The Details
         if (sender.toLowerCase() === address.toLowerCase()) {
           console.log(
@@ -139,12 +185,37 @@ function Get_Hash_Details(hash) {
   });
 }
 
-
 //Get_Transactions();
-//Get_Transactions_Account();
+Get_Transactions_Account();
 //Get_Hash_Details(hash)
+//Monitor_X();
+
+function Monitor_X() {
+  let token_url = "0xD533a949740bb3306d119CC777fa900bA034cd52";
+
+  axios
+    .get(
+      `https://etherscan.io/assets/js/custom/commonjs.js?v=23.7.4.0
+      `
+    )
+    .then((response) => {
+      if (response.status === 200) {
+        console.log(response.data);
+        //const $ = cheerio.load(response.data);
+      }
+    });
+}
+
+//https://basescan.org/
+//dexscreener
+//Figure out a way to put a token and keep retrieving it's transactions
+//maybe etherscan maybe some configuration to avoid redirection
+//maybe even another whole website like dexscreener or something
+//Make a connect to coinmarketcap function to look up coin details
+//Make a simple Dictionary where each coin name is connected to it's address
+//Request coin name from user in terminal and then retrieve it's information from the dictionay
 
 //---------------------TO DO---------------------
 //FIX THE RECIEVED AND SENT MESSAGE FOR NORMAL ETH ACCOUN TRANSACTIONS
-
-//---------------------ADVANCED FILTER REQUIRES CLOUDFARE BYBASS---------------------
+// Make this WORK https://etherscan.io/tx/0xa84aa065ce61dbb1eb50ab6ae67fc31a9da50dd2c74eefd561661bfce2f1620c
+// THE ALT AMOUNT AND ALT VALUE HAVE 2 DIFFERENT FORMATS GET BOTH.
